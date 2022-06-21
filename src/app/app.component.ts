aa993117ae2529d306937cf885da42cf207f7cb1
Shubham <samantaray.shubham423@gmail.com>
2022-06-21 22:56:21 +0530

    initial check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: ` 
+  <div><h1>{{pageTitle}}</h1>
+  <pm-product></pm-product>
+  </div>`
+
 })
+
 export class AppComponent {
-  title = 'apm-new';
+  pageTitle: String = 'Product Demo';
 }
