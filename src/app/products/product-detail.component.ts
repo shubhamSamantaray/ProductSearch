b6b8900f6978188679645f362ce5aed99b206951
Shubham Samantaray <samantaray.shubham423@gmail.com>
2022-07-20 16:46:48 +0530

    added route and guard and nav button

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   // selector: 'pm-product-detail',
@@ -7,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detail';
-  constructor() { }
+  pageTitle: string = 'Product Details';
+  product: IProduct | undefined;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `:${id}`;
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
+
