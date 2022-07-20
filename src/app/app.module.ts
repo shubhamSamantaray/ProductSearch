c69db7493980bdeb7f1d30d675a2c2f9954df593
Shubham Samantaray <samantaray.shubham423@gmail.com>
2022-07-20 18:31:48 +0530

    added modules for product and shared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,14 @@ import { FormsModule } from '@angular/forms'; //Itgot added because the app modu
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
-
+import { ProductModule } from './products/product.module';
 @NgModule({
   // local Directive or pipes are declred under declarations
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent  // the component is declared here 
+    WelcomeComponent, // the component is declared here 
   ],
   // the pipes used from 3rd party/angular is used under import
   imports: [
@@ -28,18 +19,13 @@ import { ProductDetailGuard } from './products/product-detail.guard';
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
       // the last one is a wild card one and the above one is default one
       //  Which is usually mentoioned as 404 page 
-    ])
+    ]),
+    ProductModule,
     // The route values must be passed on.
     //We need to add imports here in order to use it for the ng module decorator 
   ],
